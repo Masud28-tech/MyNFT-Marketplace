9988ffacc19355895891b47c63590b8580b94c7c
Masud28-Tech <masud.tech28@gmail.com>
2022-09-16 12:53:42 +0530

    Commit 1.5: EsLint setup is done. with eslint configurations and rules.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+);
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
+export default Home;
