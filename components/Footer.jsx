4ca92459070cadc21f406b6bba9bf6a70b71190f
Masud28-Tech <masud.tech28@gmail.com>
2022-09-21 21:50:12 +0530

    Commit 3.5: Done Creating footer (resposive)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,14 +5,31 @@ import Image from 'next/image';
 import images from '../assets';
 import Button from './Button';
 
+// CHILD COMPONENT
+const FooterLinks = ({ heading, items }) => (
+  <div className="flex-1 justify-start items-start">
+    <h3 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl mb-10">
+      {heading}
+    </h3>
+
+    {items.map((item, idx) => (
+      <p key={idx} className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3">
+        {item}
+      </p>
+    ))}
+
+  </div>
+);
+
 const Footer = () => {
   const { theme } = useTheme();
 
   return (
     <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
 
-      {/* FOOTER-SECTION 1 AND ROW 1: LOGO+NEWS-LETTER PART */}
+      {/* FOOTER-SECTION 1 */}
       <div className="w-full minmd:w-4/5 flex flex-row md:flex-col sm:px-4 px-16">
+        {/* ROW 1: LOGO+NEWS-LETTER PART  */}
         <div className="flexStart flex-1 flex-col">
 
           <div className="flexCenter cursor-pointer">
@@ -40,6 +57,11 @@ const Footer = () => {
             </div>
           </div>
 
+        </div>
+        {/* AND ROW 2: OTHER LINKS AND SERVICES PART  */}
+        <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:ml-0 md:mt-8">
+          <FooterLinks heading="MyNFT Market" items={['Explore', 'How It Works', 'Contact Us']} />
+          <FooterLinks heading="Support" items={['Help Center', 'Terms of Service', 'Legal', 'Privacy Policy']} />
         </div>
       </div>
 
