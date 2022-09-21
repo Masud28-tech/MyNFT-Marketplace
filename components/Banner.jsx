3ef856e8f4d959165611deeb8c90bd2ee017853e
Masud28-Tech <masud.tech28@gmail.com>
2022-09-21 23:53:35 +0530

    Commit 4: Create banner in home page i.e. App tag line banner/reactangular tab

---
+++ components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = ({ bannerTitle, parentStyles, childStyles }) => (
+  <div className={`${parentStyles} relative w-full flex items-center overflow-hidden nft-gradient`}>
+    <p className={`${childStyles} font-bold text-5xl font-poppins leading-70`}>
+      {bannerTitle}
+    </p>
+
+    {/* DECORATIVE CIRCLE-1 */}
+    <div className="absolute white-bg w-48 h-48 sm:w-32 sm:h-32 rounded-full -bottom-10 -left-16 z-5" />
+    {/* DECORATIVE CIRCLE-2 */}
+    <div className="absolute white-bg w-72 h-72 sm:w-56 sm:h-56 rounded-full -top-24 -right-14 z-5" />
+  </div>
+);
+
+export default Banner;
